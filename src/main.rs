699fad712d610361af2d6cf1d4dbad4ef4e184f6
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-03 13:04:54 +0700

    add the main menu loop

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,40 @@ fn get_input() -> Option<String> {
     }
 }
 
-fn main() {}
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+
+impl MainMenu {
+    fn select_menu(input: String) -> Option<MainMenu> {
+        match input.as_str() {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
+        }
+    }
+    fn show_menu() {
+        println!("");
+        println!("== Bill Manager ==");
+        println!("1. Add Bill");
+        println!("2. View Bills");
+        println!("");
+        println!("Enter Selection: ");
+    }
+}
+
+fn main() {
+    // create bill structure
+    loop {
+        // Display the menu
+        MainMenu::show_menu();
+        let input = get_input().expect("no data entered");
+        match MainMenu::select_menu(input) {
+            Some(MainMenu::AddBill) => (),
+            Some(MainMenu::ViewBill) => (),
+            None => return,
+        }
+        // Make a choice, based on user input
+    }
+}
